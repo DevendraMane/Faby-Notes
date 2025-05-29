129c0af23b94f7c4affe154b802957dd355418ac
DevendraMane <devendra988196@gmail.com>
2025-05-30 00:12:40 +0530

    server.js updated

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 // ***** HANDLING CORS ***** //
 const corsOptions = {
-  origin: `http://localhost:5173`,
+  origin: `https://faby-clean-client.onrender.com/home`,
   methods: `GET, POST, PUT, DELETE, PATCH, HEAD`,
   credentials: true,
 };
